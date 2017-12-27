0cb0f48991d0b0efe9099366dd862df7bf83640b
oskov <vladimirs.shafrans@inbox.lv>
2017-12-27 17:31:07 +0200

    00

---
+++ core/src/com/td/game/WorldLogic.java
@@ -0,0 +1,33 @@
+package com.td.game;
+
+import java.util.LinkedList;
+
+import com.td.Entity.Entity;
+import com.td.screens.GameScreen;
+import com.td.util.Const;
+
+public class WorldLogic {
+private GameScreen game;
+private int playerHp;
+private int playerMoney;
+private int[][] gameField;
+private int lvl;
+private LinkedList<Entity> weaponList;
+private LinkedList<Entity> mobList;
+
+	public WorldLogic(GameScreen g) {
+		game=g;
+		playerHp=Const.START_HP;
+		playerMoney=Const.START_CASH;
+		weaponList=new LinkedList();
+		mobList=new LinkedList();
+		
+	}	
+	
+	public void update() {
+		
+		
+		
+	}
+	
+}
