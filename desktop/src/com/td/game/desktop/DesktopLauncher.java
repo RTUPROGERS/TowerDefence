747197d4582afaa160a13560e054f5c3d6e08cd4
denis <denja320@gmail.com>
2018-01-22 15:12:32 +0200

    разрешение
    поменял разрешение, птм что на маке за экран вылазит

--- desktop/src/com/td/game/desktop/DesktopLauncher.java
+++ desktop/src/com/td/game/desktop/DesktopLauncher.java
@@ -9,8 +9,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.fullscreen=false;
-		config.height=1080;
-		config.width=1920;
+		config.height=768;
+		config.width=1024;
 		new LwjglApplication(new GameClass(), config);
 	}
 }
